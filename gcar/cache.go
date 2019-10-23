d39bb6b37e941bd8cafafbbc6a54a379e1e859c5
aorjoa <bhuridech@gmail.com>
2019-10-23 13:19:53 +0700

    modify struct item name and employ mutex lock on get method

--- gcar/cache.go
+++ gcar/cache.go
@@ -1,9 +1,11 @@
 package gcar
 
-// Item use for construct model to store cache
-type Item struct {
-	Object     interface{}
-	Expiration int64
+import "sync"
+
+// Cache use for construct model to store cache
+type Cache struct {
+	mu    sync.RWMutex
+	items map[string]interface{}
 }
 
 // Set should be set cache to memory
@@ -12,6 +14,12 @@ func Set() (string, bool) {
 }
 
 // Get should be get cache data from memory
-func Get() (string, bool) {
-	return "Hello, World", true
+func (c *Cache) Get(k string) (interface{}, bool) {
+	c.mu.RLock()
+	item, found := c.items[k]
+	if !found {
+		c.mu.RUnlock()
+		return nil, false
+	}
+	return item, true
 }
